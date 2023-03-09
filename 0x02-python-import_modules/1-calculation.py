5923164fa4f3fc4738c127b8fea8788c8fa4b461
Becky1703 <oluwabukunmia@gmail.com>
2023-03-09 07:31:08 -0800

    made changes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print all the imported modules"""
     from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
@@ -8,3 +7,4 @@ print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
+
