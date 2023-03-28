d69dc3fb31547c7d311ee67180694cdfde61e22f
Becky1703 <oluwabukunmia@gmail.com>
2023-03-28 07:51:46 -0700

    added new files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+
+
+class Square:
+
+    """Initializes the square."""
+    def __init__(self, size=0):
+
+        """The class Square takes in
+
+        arguements
+
+        """
+
+        self.__size = size
