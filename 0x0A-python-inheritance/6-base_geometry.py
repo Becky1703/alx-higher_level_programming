06013a433040e1e96986e20dd72707cf244443d2
Becky1703 <oluwabukunmia@gmail.com>
2023-04-11 05:06:54 -0700

    added new files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that defines a class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
