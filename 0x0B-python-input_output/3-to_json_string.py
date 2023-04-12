5c0aa5c1be5647eddaf6923118fbf958a9bb611f
Becky1703 <oluwabukunmia@gmail.com>
2023-04-12 07:27:05 -0700

    corrected pycodestyle error

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Function to convert string to JSON representation"""
 import json
 
+
 def to_json_string(my_obj):
     """The function converts a object (string)
     and returns the JSON representation.
