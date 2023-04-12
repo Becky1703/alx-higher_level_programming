c513a66603298b46b219bb972e440c1ba5b5315e
Becky1703 <oluwabukunmia@gmail.com>
2023-04-12 07:23:32 -0700

    added new files

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function to convert string to JSON representation"""
+import json
+
+def to_json_string(my_obj):
+    """The function converts a object (string)
+    and returns the JSON representation.
+    """
+    return json.dumps(my_obj)
