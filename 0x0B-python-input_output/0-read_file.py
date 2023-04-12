5b35d73d8dbb6891123639c95243f0cf65a3aefa
Becky1703 <oluwabukunmia@gmail.com>
2023-04-12 06:40:28 -0700

    made changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """the function reads a text from file and prints to stdout"""
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
