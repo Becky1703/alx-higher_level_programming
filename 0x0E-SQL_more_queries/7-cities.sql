46afbfe7fdc29850f3487b8e8a943be8edc0d203
Becky1703 <oluwabukunmia@gmail.com>
2023-05-17 16:13:30 -0700

    fixed error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- script creates the database and table
 -- creates database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
 -- creates table
 CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
