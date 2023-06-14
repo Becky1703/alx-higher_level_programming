81888b21354bb6a7e2a7a80e74c0c580555d0b36
Becky1703 <oluwabukunmia@gmail.com>
2023-06-14 06:23:42 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.heigt; i++) {
+    for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
