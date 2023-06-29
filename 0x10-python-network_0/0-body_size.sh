ca3e7d597415bdcb6e2eec0431ed1afe3c333b57
Becky1703 <oluwabukunmia@gmail.com>
2023-06-29 02:47:05 -0700

    curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #Takes in a URL, sends request to the URL and displays size of the body in bytes
 curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Takes a URL, sends GET request to the URL, and displays body of a 200 status code
+curl -sL "$1"
+
