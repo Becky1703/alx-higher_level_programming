cb2832b21865e2da5ff1ac86ebaf466e5e8779c9
Becky1703 <oluwabukunmia@gmail.com>
2023-06-29 02:19:35 -0700

    port 5000

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 #Takes in a URL, sends request to the URL and siplays size of the body in bytes
-#+ using curl
+#+ using curl, and testing on a server running on port 5000
 
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
