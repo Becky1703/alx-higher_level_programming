78877169337c7f982ccc396406afd5ea84cf0ea9
root <oluwabukunmia@gmail.com>
2023-07-02 08:07:18 -0700

    post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The script sends POST request"""
+import requests
+from sys import argv
+
+def display_request(url: str, email_arg:str):
+    payload = {"email": email_arg}
+    req = requests.post(url, data=payload)
+    print(req.text)
+
+
+if __name__ == "__main__":
+    display_request(argv[1], argv[2])
